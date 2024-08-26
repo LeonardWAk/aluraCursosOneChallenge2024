fdc56bbb90a69af1cc85cabce1e60c1824ad59c9
LeonardWAK <xl3132@gmail.com>
2024-08-26 05:52:47 -0300

    finalVersion__02

--- encript.js
+++ encript.js
@@ -3,16 +3,25 @@ const codigoEL = document.getElementById("codigoEL");
 const btnEL = document.getElementById("btnEL");
 const btnEL2 = document.getElementById("btnEL2");
 const copyButton = document.getElementById("btn__copy");
-
+const errorMessage = document.getElementById("errorMessage");
 
 const letters = ['e', 'i', 'a', 'o', 'u'];
 const keys = ['enter', 'imes', 'ai', 'ober', 'ufat'];
 
+// Función para validar si el texto está en minúsculas y sin tildes
+function isValidText(text) {
+  // Expresión regular para letras minúsculas sin acentos
+  const regex = /^[a-z\s]+$/;
+
+  return regex.test(text);
+}
 
 function encriptar(text) {
     let textoEncriptado = [];
-    
-    for (let i = 0; i < text.length; i++) {
+
+    if (isValidText(text)) {
+
+      for (let i = 0; i < text.length; i++) {
         let letraEncontrada = false;
         for (let j = 0; j < letters.length; j++) {
             if (text[i] === letters[j]) {
@@ -27,6 +36,16 @@ function encriptar(text) {
     }
 
     return textoEncriptado.join('');
+     
+  } else {
+      // Mostrar el mensaje de error
+      errorMessage.style.display = 'block';
+
+      // Ocultar el mensaje de error después de 2 segundos
+      setTimeout(function() {
+          errorMessage.style.display = 'none';
+      }, 2000);
+  }
 }
 
 function desencriptar(text) {
@@ -58,16 +77,15 @@ copyButton.addEventListener('click', function() {
   navigator.clipboard.writeText(textCopy)
       .then(function() {
           console.log('Texto copiado al portapapeles.');
-          copyMessage.style.display = 'block';
+          message.style.display = 'block';
           setTimeout(function() {
-              copyMessage.style.display = 'none';
+              message.style.display = 'none';
           }, 2000);
       })
       .catch(function(err) {
           console.error('Error al copiar el texto: ', err);
       });
 });
-
 
 btnEL.addEventListener("click", (e) => {
   e.preventDefault();
